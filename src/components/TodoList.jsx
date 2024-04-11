7c0e09e2bff8139d0a1a8b32a3a7310d6604d44e
Pernilla Sterner <psterner@hotmail.com>
2024-04-11 08:36:13 +0200

    Basic design

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,8 +8,11 @@ import { useTodo } from "../contexts/TodoContext";
  * - update
  * - remove
  *
- * Access
- * - fetch all todos
+ * Fetch
+ * - fetch all todos ✅
+ *
+ * Todos
+ * - Only display list when there are todos added ✅
  */
 
 export const TodoList = () => {
@@ -18,42 +21,46 @@ export const TodoList = () => {
   console.log(todos);
 
   return (
-    <div className="todo-list-wrapper">
-      <p className="totals">Total of tasks: {todos.length}</p>
-      <div className="todo-section uncompleted-section">
-        <h3>Uncompleted tasks</h3>
-        {/* Mapp all uncompleted lists */}
-        {todos.map(
-          (todo) =>
-            !todo.isCompleted && (
-              <div key={todo.id} className="todo-wrapper">
-                <form>
-                  <input type="checkbox" />
-                  <p className="description">{todo.description}</p>
-                  {/* This button should say Save after clicking on update */}
-                  <button className="update-btn">Update</button>
-                </form>
-              </div>
-            )
-        )}
+    <>
+      {todos.length > 0 && (
+        <div className="todo-list-wrapper">
+          <p className="totals">Total of tasks: {todos.length}</p>
+          <div className="todo-section uncompleted-section">
+            <h3>Uncompleted tasks</h3>
+            {/* Mapp all uncompleted lists */}
+            {todos.map(
+              (todo) =>
+                !todo.isCompleted && (
+                  <div key={todo.id} className="todo-wrapper">
+                    <form>
+                      <input type="checkbox" />
+                      <p className="description">{todo.description}</p>
+                      {/* This button should say Save after clicking on update */}
+                      <button className="update-btn">Update</button>
+                    </form>
+                  </div>
+                )
+            )}
 
-        <div className="todo-section completed-section">
-          <h3>Completed tasks</h3>
-          {/* Mapp all completed lists */}
-          {todos.map(
-            (todo) =>
-              todo.isCompleted && (
-                <div key={todo.id} className="todo-wrapper">
-                  <form>
-                    <input type="checkbox" />
-                    <p className="description">{todo.description}</p>
-                    <button className="delete-btn">Delete</button>
-                  </form>
-                </div>
-              )
-          )}
+            <div className="todo-section completed-section">
+              <h3>Completed tasks</h3>
+              {/* Mapp all completed lists */}
+              {todos.map(
+                (todo) =>
+                  todo.isCompleted && (
+                    <div key={todo.id} className="todo-wrapper">
+                      <form>
+                        <input type="checkbox" checked />
+                        <p className="description">{todo.description}</p>
+                        <button className="delete-btn">Delete</button>
+                      </form>
+                    </div>
+                  )
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };

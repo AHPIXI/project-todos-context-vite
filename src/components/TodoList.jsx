c99dcba8c0d0c93a7ef4fedef83495f52f1dd007
Pernilla Sterner <psterner@hotmail.com>
2024-04-11 09:49:13 +0200

    Be able to check or uncheck a todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,7 +4,7 @@ import { useTodo } from "../contexts/TodoContext";
 /**
  *
  * Actions
- * - completed / uncompleted
+ * - completed / uncompleted ✅
  * - update
  * - remove
  *
@@ -13,12 +13,23 @@ import { useTodo } from "../contexts/TodoContext";
  *
  * Todos
  * - Only display list when there are todos added ✅
+ * - Add tasks into localstorage.
  */
 
 export const TodoList = () => {
   // Fetch all todos from globel state
-  const { todos } = useTodo();
-  console.log(todos);
+  const { todos, updateTodo } = useTodo();
+
+  // Function that handles uncompleted or completed todos
+  const handleCheckboxChange = (id) => {
+    // Create a new variable for updated todos and loop throught existing todos to find todo with matching id.
+    // Set the matching todo to the opposite value. If completed set to uncompleted and vice versa.
+    const updatedTodos = todos.map((todo) =>
+      // This is a ternary operator. "condition ? if value is true : if value is false"
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    updateTodo(updatedTodos);
+  };
 
   return (
     <>
@@ -33,7 +44,12 @@ export const TodoList = () => {
                 !todo.isCompleted && (
                   <div key={todo.id} className="todo-wrapper">
                     <form>
-                      <input type="checkbox" />
+                      <input
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        // Listen to changes and pass the id as an argument to handleCheck.. funtion
+                        onChange={() => handleCheckboxChange(todo.id)}
+                      />
                       <p className="description">{todo.description}</p>
                       {/* This button should say Save after clicking on update */}
                       <button className="update-btn">Update</button>
@@ -50,7 +66,11 @@ export const TodoList = () => {
                   todo.isCompleted && (
                     <div key={todo.id} className="todo-wrapper">
                       <form>
-                        <input type="checkbox" checked />
+                        <input
+                          type="checkbox"
+                          checked
+                          onChange={() => handleCheckboxChange(todo.id)}
+                        />
                         <p className="description">{todo.description}</p>
                         <button className="delete-btn">Delete</button>
                       </form>
